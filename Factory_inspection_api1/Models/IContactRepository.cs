a01b6a7baeb63514c65177e15be693bfb7ee662a
lijing99002008 <lijing99002008@sina.cn>
2018-02-05 15:19:44 +0800

    修改

---
+++ Factory_inspection_api1/Models/IContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using Factory_inspection_api1.Controllers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_inspection_api1.Models
+{
+    interface IContactRepository
+    {
+        IEnumerable GetAllContacts();
+        Contact GetContact(string id);
+        Contact AddContact(Contact item);
+        bool RemoveContact(string id);
+        bool UpdateContact(string id, Contact item);
+    }
+}
